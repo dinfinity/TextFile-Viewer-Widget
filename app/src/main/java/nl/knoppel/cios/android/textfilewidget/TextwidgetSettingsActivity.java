26d2d548aff6958fa43278d24be0fa1fad49b309
F.L.A.Knoppel <dualinfinity@gmail.com>
2020-01-14 23:09:19 +0100

    - Readd some content uri based code

--- app/src/main/java/nl/knoppel/cios/android/textfilewidget/TextwidgetSettingsActivity.java
+++ app/src/main/java/nl/knoppel/cios/android/textfilewidget/TextwidgetSettingsActivity.java
@@ -5,20 +5,26 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
+import android.provider.MediaStore;
 import android.util.Log;
 
 import com.lamerman.FileDialog;
+
+import java.io.IOException;
 
 public class TextwidgetSettingsActivity extends PreferenceActivity {
 
     public static final String WIDGET_PREFS_PREFIX = "textWidget_";
     public static final String PREF_FILE_PATH = "filePath";
     protected static final int FILE_CHOICE_PATH = 1;
+    protected static final int FILE_CHOICE_URI = 2;
     private int widgetId;
     private SharedPreferences sharedPreferences;
     private Editor editor;
@@ -52,9 +58,20 @@ public class TextwidgetSettingsActivity extends PreferenceActivity {
 
             public boolean onPreferenceClick(Preference preference) {
 
-                Intent intent = new Intent(getBaseContext(), FileDialog.class);
-                intent.putExtra(FileDialog.START_PATH, path);
-                startActivityForResult(intent, FILE_CHOICE_PATH);
+                boolean useFileSelector = true;
+                if (useFileSelector) {
+                    Intent intent = new Intent(getBaseContext(), FileDialog.class);
+                    intent.putExtra(FileDialog.START_PATH, path);
+                    startActivityForResult(intent, FILE_CHOICE_PATH);
+                } else {
+                    // TODO [High] We need a FileObserver for actual file change observe actions elsewhere, but we need to work with URIs in all other circumstances
+                    // See other branch
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("text/*");
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent = Intent.createChooser(intent, "Choose a file");
+                    startActivityForResult(intent, FILE_CHOICE_URI);
+                }
 
                 return true;
             }
@@ -70,6 +87,16 @@ public class TextwidgetSettingsActivity extends PreferenceActivity {
                 }
 
                 break;
+            case FILE_CHOICE_URI:
+                if (resultCode != RESULT_CANCELED) {
+                    Uri uri = data.getData();
+                    assert uri != null;
+                    Log.i(TextwidgetProvider.TAG, "Got uri: " + uri.toString());
+
+                    updateUri(uri);
+                }
+
+                break;
         }
     }
 
@@ -81,6 +108,44 @@ public class TextwidgetSettingsActivity extends PreferenceActivity {
         Log.d(TextwidgetProvider.TAG, "Stored: " + path);
         String newStoredPath = sharedPreferences.getString(PREF_FILE_PATH, "No file selected");
         fileSelectPreference.setSummary(newStoredPath);
+    }
+
+    private void updateUri(Uri uri) {
+        String path = getRealPathFromURI(uri);
+        updatePath(path);
+    }
+
+    /**
+     * Tries to convert an uri to a file path, using the content resolver of the context.
+     * TODO [High] Deprecated
+     *
+     * @deprecated Probably doesn't work anymore in Android 10
+     *
+     * @param uri
+     * @return
+     */
+    private String getRealPathFromURI(Uri uri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, proj, null, null, null);
+
+        String path = uri.getPath();
+
+        if (cursor != null) {
+            boolean hasResults = cursor.moveToFirst();
+            if (hasResults) {
+                int index = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+                path = cursor.getString(index);
+            } else {
+                Log.i(TextwidgetProvider.TAG, "No data found for contentUri: " + uri.toString());
+            }
+            cursor.close();
+        }
+
+        if (path == null) {
+            Log.i(TextwidgetProvider.TAG, "Path returned by realPathFromUri null for contentUri: " + uri.toString());
+        }
+
+        return path;
     }
 
     public void onPause() {
